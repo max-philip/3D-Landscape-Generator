ee178c11d143dd25585c2e9444b73c5dbf205208
Max Philip <mphilip1@student.unimelb.edu.au>
2018-08-12 00:03:20 +1000

    free camera movement working

---
+++ Project1_Landscape/Assets/flyingCamera.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flyingCamera : MonoBehaviour
+{
+    float speed = 20f;
+    private Vector3 prevMouse;
+    float sensitivity = 0.2f;
+
+    void Update()
+    {
+
+        // take in mouse input and change camera perspective
+        prevMouse = Input.mousePosition - prevMouse;
+        prevMouse = new Vector3(-prevMouse.y * sensitivity, prevMouse.x * sensitivity, 0);
+        prevMouse = new Vector3(transform.eulerAngles.x + prevMouse.x, transform.eulerAngles.y + prevMouse.y, 0);
+        transform.eulerAngles = prevMouse;
+        prevMouse = Input.mousePosition;
+
+        // take keyboard input and update position
+        Vector3 pos = keyboardDir();
+        pos = pos * Time.deltaTime * speed;   
+        transform.Translate(pos);
+        
+
+    }
+
+    // take in keyboard input (both arrows and WASD)
+    private Vector3 keyboardDir()
+    {
+        Vector3 p_Velocity = new Vector3();
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            p_Velocity += new Vector3(1, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            p_Velocity += new Vector3(-1, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            p_Velocity += new Vector3(0, 0, 1);
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            p_Velocity += new Vector3(0, 0, -1);
+        }
+        return p_Velocity;
+    }
+}
