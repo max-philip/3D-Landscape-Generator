ac7749f4aa72c7d4297b293351ef8df7ea8b52a7
Max Philip <mphilip1@student.unimelb.edu.au>
2018-08-19 04:31:10 +1000

    smol changes

---
+++ Project1_Landscape/Assets/sunOrbit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sunOrbit : MonoBehaviour {
+
+    private float dayCycleSpeed = 15f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(Vector3.zero, Vector3.right, dayCycleSpeed * Time.deltaTime);
+        transform.LookAt(Vector3.zero);
+	}
+}
